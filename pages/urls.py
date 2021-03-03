457e9e3aa52a4826b62a0c15c9bdd84c769cb66d
ceortiz33 <cerwinortiz@gmail.com>
2021-03-03 16:52:55 -0500

    feat: Route Page Creation

--- pages/urls.py
+++ pages/urls.py
@@ -7,6 +7,15 @@ app_name = 'pages'
 urlpatterns = [
 	#Page that show home page
 	path('',views.home, name='home'),
+	path('home/', views.home, name='home'),
 	#Page that show about section.
 	path('about/', views.about, name='about'),
+	#Page that show services section
+	path('services/',views.services, name='services'),
+	#Page that show skills section
+	path('skills/',views.skills, name='skills'),
+	#Page that show teams section
+	path('teams/',views.teams, name='teams'),
+	#Page that show contacts section
+	path('contacts/',views.contacts, name='contacts'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -8,3 +8,19 @@ def home(request):
 def  about(request):
 	"""The about page of my portfolio"""
 	return render(request,"pages/about.html")
+
+def services(request):
+	"""The services page of my portofolio"""
+	return render(request,"pages/services.html")
+
+def skills(request):
+	"""The skills page of my portfolio"""
+	return render(request,"pages/skills.html")
+
+def teams(request):
+	"""The teams page of my portfolio"""
+	return render(request,"pages/teams.html")
+
+def contacts(request):
+	"""The contacts page of my portfolio"""
+	return render(request,"pages/contacts.html")
